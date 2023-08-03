b03b2b5e1b15230a94ffde7738ff6ac05a84fbb3
MSreeN <khyathimnsree369@gmail.com>
2023-08-03 22:01:17 +0530

    added addTodo form and redirected to todos page

--- src/main/java/com/learn/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/learn/springboot/myfirstwebapp/todo/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -19,5 +20,17 @@ public class TodoController {
     public String getTodoByUserName(ModelMap todo, HttpSession session){
         session.getAttribute("name"); 
         todo.put("todos",todoService.findByUserName("Sree"));
-        return "Todos"; }    
+        return "Todos";
+    }
+
+    @GetMapping("/add-todo")
+    public String showAddTodoPage(){
+        return "AddTodo";
+    }
+
+    @PostMapping("/newTodo")
+    public String addTodo(){
+        return "redirect:listTodo";
+    }
+
 }
