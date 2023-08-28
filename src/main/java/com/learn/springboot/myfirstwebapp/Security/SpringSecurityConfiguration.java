90c0ee4b97ac3bc25e1b48d530e168e5134aa0c7
MSreeN <khyathimnsree369@gmail.com>
2023-08-28 21:46:19 +0530

    getting userame form SecurityContextHolder

--- src/main/java/com/learn/springboot/myfirstwebapp/Security/SpringSecurityConfiguration.java
+++ src/main/java/com/learn/springboot/myfirstwebapp/Security/SpringSecurityConfiguration.java
@@ -2,6 +2,8 @@ package com.learn.springboot.myfirstwebapp.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,6 +27,11 @@ public class SpringSecurityConfiguration {
                 .build();
 
         return  new InMemoryUserDetailsManager(userDetails);
+    }
+
+    public String getLoggedInUsername(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
     }
 
     @Bean
--- src/main/java/com/learn/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/learn/springboot/myfirstwebapp/login/LoginController.java
@@ -1,6 +1,8 @@
 package com.learn.springboot.myfirstwebapp.login;
 
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
     @RequestMapping("/")
     public String Greeting(ModelMap modelMap){
-        modelMap.put("name", "Sree");
+        modelMap.put("name", getLoggedInUserName());
         return "Welcome";
+    }
+
+    public String getLoggedInUserName(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
     }
 }
